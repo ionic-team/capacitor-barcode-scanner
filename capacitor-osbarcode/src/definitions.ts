bdad2d141931238c68757ce0c9497d7f53afb4f1
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2024-01-29 15:12:22 -0500

    chore: stub web defs

--- capacitor-osbarcode/src/definitions.ts
+++ capacitor-osbarcode/src/definitions.ts
@@ -1,3 +1,13 @@
+export type OSBarcodeScanOptions = { 
+  value: string, 
+  androidScanningLibrary: 'ZXing' | 'MLKit' 
+}
+
+export type OSBarcodeScanOptionsLegacy = Omit<OSBarcodeScanOptions, 'androidScanningLibrary'>;
+
+export type OSBarcodeScanResult = { ScanResult: string };
+
 export interface OSBarcodePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  scanBarcode(options: OSBarcodeScanOptions): Promise<OSBarcodeScanResult>;
+  scan(options: OSBarcodeScanOptionsLegacy): Promise<OSBarcodeScanResult>;
 }
--- capacitor-osbarcode/src/web.ts
+++ capacitor-osbarcode/src/web.ts
@@ -1,10 +1,21 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { OSBarcodePlugin } from './definitions';
+import type { 
+  OSBarcodePlugin, 
+  OSBarcodeScanOptions, 
+  OSBarcodeScanOptionsLegacy, 
+  OSBarcodeScanResult 
+} from './definitions';
 
 export class OSBarcodeWeb extends WebPlugin implements OSBarcodePlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  
+  async scanBarcode(options: OSBarcodeScanOptions): Promise<OSBarcodeScanResult> {
+    const str = 'ECHO: ' + JSON.stringify(options);
+    console.log(str);
+    return { ScanResult: str };
+  }
+
+  async scan(options: OSBarcodeScanOptionsLegacy): Promise<OSBarcodeScanResult> {
+    return await this.scanBarcode({ ...options, androidScanningLibrary: 'ZXing' });
   }
 }

59d9fc5b1eb8fe65a3a1fd6d8cf361666d27154f
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2024-05-16 15:56:52 -0400

    fix: use defaults where needed

--- example-app/src/pages/Home.tsx
+++ example-app/src/pages/Home.tsx
@@ -1,26 +1,14 @@
 import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import { useState } from 'react';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint, CapacitorBarcodeScannerCameraDirection, CapacitorBarcodeScannerScanOrientation, CapacitorBarcodeScannerAndroidScanningLibrary } from '@capacitor/barcode-scanner';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
 
 const Home: React.FC = () => {
   const [scannerResult, setScannerResult] = useState<string>('No Data...');
 
   const scanBarcode = async () => {
     const result = await CapacitorBarcodeScanner.scanBarcode({
-      hint: CapacitorBarcodeScannerTypeHint.ALL,
-      scanInstructions: 'Hold your device over the barcode to scan.',
-      scanButton: true, // TODO: Not implemented yet in the web implementation
-      scanText: 'Press To Start Scanning', // TODO: Not implemented yet in the web implementation
-      cameraDirection: CapacitorBarcodeScannerCameraDirection.BACK, 
-      scanOrientation: CapacitorBarcodeScannerScanOrientation.ADAPTIVE, // TODO: Not implemented yet in the web implementation
-      android: {
-        scanningLibrary: CapacitorBarcodeScannerAndroidScanningLibrary.MLKIT,
-      },
-      web: {
-        showCameraSelection: false, // TODO: Not implemented yet in the web implementation
-        scannerFPS: 30
-      }
+      hint: CapacitorBarcodeScannerTypeHint.ALL
     });
     setScannerResult(result.ScanResult);
   };
--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,6 +1,12 @@
 import { registerPlugin } from '@capacitor/core';
 
-import type { CapacitorBarcodeScannerPlugin } from './definitions'; // Importing the interface for type checking.
+import {
+  CapacitorBarcodeScannerCameraDirection,
+  CapacitorBarcodeScannerScanOrientation,
+  type CapacitorBarcodeScannerOptions,
+  type CapacitorBarcodeScannerPlugin,
+  type CapacitorBarcodeScannerScanResult,
+} from './definitions'; // Importing the interface for type checking.
 import { applyCss, barcodeScannerCss } from './utils'; // Import utilities for applying CSS.
 
 /**
@@ -8,12 +14,23 @@ import { applyCss, barcodeScannerCss } from './utils'; // Import utilities for a
  * For web platforms, it applies necessary CSS for the barcode scanner and dynamically imports the web implementation.
  * This allows for lazy loading of the web code only when needed, optimizing overall bundle size.
  */
-const CapacitorBarcodeScanner = registerPlugin<CapacitorBarcodeScannerPlugin>('CapacitorBarcodeScanner', {
+const CapacitorBarcodeScannerImpl = registerPlugin<CapacitorBarcodeScannerPlugin>('CapacitorBarcodeScanner', {
   web: () => {
     applyCss(barcodeScannerCss); // Apply the CSS styles necessary for the web implementation of the barcode scanner.
     return import('./web').then((m) => new m.CapacitorBarcodeScannerWeb()); // Dynamically import the web implementation and instantiate it.
   },
 });
 
+class CapacitorBarcodeScanner {
+  public static async scanBarcode(options: CapacitorBarcodeScannerOptions): Promise<CapacitorBarcodeScannerScanResult> {
+    options.scanInstructions = options.scanInstructions || ' '; // Ensure scanInstructions is at least a space.
+    options.scanButton = options.scanButton || false; // Set scanButton to false if not provided.
+    options.scanText = options.scanText || ' '; // Ensure scanText is at least a space.
+    options.cameraDirection = options.cameraDirection || CapacitorBarcodeScannerCameraDirection.BACK; // Set cameraDirection to 'BACK' if not provided.
+    options.scanOrientation = options.scanOrientation || CapacitorBarcodeScannerScanOrientation.ADAPTIVE; // Set scanOrientation to 'ADAPTIVE' if not provided.
+    return CapacitorBarcodeScannerImpl.scanBarcode(options);
+  }
+}
+
+export { CapacitorBarcodeScanner }; // Export the `CapacitorBarcodeScanner` class.
 export * from './definitions'; // Re-export all exports from the definitions file.
-export { CapacitorBarcodeScanner }; // Export the OSBarcode plugin for use in Capacitor projects.

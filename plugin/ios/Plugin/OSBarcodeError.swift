a100a28e548d957208d7680b9c54eec27ed39841
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2025-03-13 09:53:44 +0000

    fix: use OSBarcodeError class to handle error cases, using a description and code, instead of just sending an error message

--- plugin/ios/Plugin/OSBarcodeError.swift
+++ plugin/ios/Plugin/OSBarcodeError.swift
@@ -11,15 +11,21 @@ enum OSBarcodeError: Int, CustomNSError, LocalizedError {
     case scanningCancelled = 6
     case cameraAccessDenied = 7
     case scanInputArgumentsIssue = 8
+    case bridgeNotInitialized = 13
 
-    var errorDescription: String? {
+    var errorDescription: String {
         switch self {
         case .scanningError: return "Error while trying to scan code."
         case .scanningCancelled: return "Couldn’t scan because the process was cancelled."
         case .cameraAccessDenied: return "Couldn’t scan because camera access wasn’t provided. Check your camera permissions and try again."
         case .scanInputArgumentsIssue: return "Scanning parameters are invalid."
+        case .bridgeNotInitialized: return "Capacitor bridge or viewController is not initialized."
         }
     }
+    
+    var errorCode: String {
+            return "OS-PLUG-BARC-\(String(format: "%04d", self.rawValue))"
+        }
 
     var errorDictionary: [String: String] {
         [

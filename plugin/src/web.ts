72fa47ac04107984420d3e5c88a4a5b25a5cc0c3
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-09-02 12:25:46 +0100

    chore: fix lint issues.

--- plugin/src/web.ts
+++ plugin/src/web.ts
@@ -128,7 +128,11 @@ export class CapacitorBarcodeScannerWeb extends WebPlugin implements CapacitorBa
       };
 
       const OSBarcodeWebScannerErrorCallback = (error: string) => {
-        const allowedErrors = ['NotFoundException', 'No barcode or QR code detected', 'No MultiFormat Readers were able to detect the code'];
+        const allowedErrors = [
+          'NotFoundException',
+          'No barcode or QR code detected',
+          'No MultiFormat Readers were able to detect the code',
+        ];
 
         if (!allowedErrors.find((e) => error.indexOf(e) !== -1)) {
           this.stopAndHideScanner();

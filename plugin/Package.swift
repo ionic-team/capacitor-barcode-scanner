c098c4aeaac118c70a5f79dd018533abd8610956
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2026-02-06 17:24:59 +0000

    fix(ios): Missing image resources with SPM (#115)

--- plugin/Package.swift
+++ plugin/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
-        .package(url: "https://github.com/OutSystems/OSBarcodeLib-iOS.git", exact: "2.1.0")
+        .package(url: "https://github.com/OutSystems/OSBarcodeLib-iOS.git", from: "2.1.1")
     ],
     targets: [
         .target(

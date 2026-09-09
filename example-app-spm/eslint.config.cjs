db2c9ac790d572e2d6fa354e17c37442dbeb15e4
Chace Daniels <chace.daniels@outsystems.com>
2026-09-09 09:25:58 -0500

    chore: update eslint to v10 and @ionic/eslint-config to v1

---
+++ example-app-spm/eslint.config.cjs
@@ -0,0 +1,21 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  { ignores: ['dist/**', 'build/**'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+  },
+);
--- example-app/src/components/ExploreContainer.tsx
+++ example-app/src/components/ExploreContainer.tsx
@@ -1,8 +1,7 @@
 import './ExploreContainer.css';
 
-interface ContainerProps { }
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
+const ExploreContainer: React.FC = () => {
   return (
     <div id="container">
       <strong>Ready to create an app?</strong>

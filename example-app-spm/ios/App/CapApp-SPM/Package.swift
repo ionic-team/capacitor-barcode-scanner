290f941de17894ca18fc3e908c09d2a91379297a
jcesarmobile <jcesarmobile@gmail.com>
2026-09-22 15:43:28 +0200

    update capacitor dep in spm app

--- example-app-spm/ios/App/CapApp-SPM/Package.swift
+++ example-app-spm/ios/App/CapApp-SPM/Package.swift
@@ -11,14 +11,14 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "9.0.0-alpha.6"),
+        .package(url: "https://github.com/ionic-team/capacitor", exact: "9.0.0-alpha.7"),
         .package(name: "CapacitorBarcodeScanner", path: "../../../../plugin")
     ],
     targets: [
         .target(
             name: "CapApp-SPM",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Capacitor", package: "capacitor"),
                 .product(name: "CapacitorBarcodeScanner", package: "CapacitorBarcodeScanner")
             ]
         )
